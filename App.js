731909adb49265eede95324a22f207bb333aa512
wesling18 <bermudezwesling@gmail.com>
2025-10-22 01:29:52 -0600

    Cambios en app.js

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { FavoritosProvider } from './FavoritosContext';
+import Navegacion from './Navegacion';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <FavoritosProvider>
+      <Navegacion />
+    </FavoritosProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
